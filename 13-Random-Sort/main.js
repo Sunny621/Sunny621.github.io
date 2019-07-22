199744a0545a739897358c81496b0ebf1d3ac781
Sunny Cao <sunnynz2016@gmail.com>
2019-07-22 22:57:40 +1200

    Update Random Sort with new dev method

--- 13-Random-Sort/main.js
+++ 13-Random-Sort/main.js
@@ -1,50 +1,61 @@
+var data = [{
+    title: "1-温馨办公",
+    img: "images/1.png"
+}, {
+    title: "2-游戏桌面",
+    img: "images/2.png"
+}, {
+    title: "3-汽车摇滚",
+    img: "images/3.png"
+}, {
+    title: "4-狭路相逢",
+    img: "images/4.png"
+}, {
+    title: "5-棒球小子",
+    img: "images/5.png"
+}, {
+    title: "6-球鞋动力",
+    img: "images/6.png"
+}, {
+    title: "7-游戏官网",
+    img: "images/7.png"
+}, {
+    title: "8-指尖社区",
+    img: "images/8.png"
+}];
+
 (function() {
-    var btns = document.querySelectorAll(".nav a");
-    var lis = document.querySelectorAll("ul li");
-    var ul = document.querySelector("ul");
+    var list = document.querySelector("#list");
+    var navs = document.querySelectorAll(".nav a");
+    var isReverse = false; // false 代表从小到大，true 从大到小
 
-    var lisArr = Array.from(lis);
-    var onOff = true;
+    render();
 
-    btns[0].onclick = function() {
-        if (onOff) {
-            this.innerHTML = "从大到小";
-            sort();
-            onOff = false;
-        } else {
-            this.innerHTML = "从小到大";
-            sort();
-            onOff = true;
-        }
-    }
-
-    btns[1].onclick = function() {
-        randomSort();
-    }
-
-    function sort() {
-        lisArr.sort(function(li1, li2) {
-            var p1 = li1.querySelector("p");
-            var p2 = li2.querySelector("p");
-            if (onOff) {
-                return p1.innerHTML.charAt(0) - p2.innerHTML.charAt(0);
-            } else {
-                return p2.innerHTML.charAt(0) - p1.innerHTML.charAt(0);
-            }
-        })
-
-        lisArr.forEach(function(element, index) {
-            ul.append(element);
+    function render() {
+        var inner = data.map(function(item, index) {
+            return '<li><img src="' + item["img"] + '" alt=""><p>' + item["title"] + '</p></li>';
         });
+
+        list.innerHTML = inner.join("");
     }
 
-    function randomSort() {
-        lisArr.sort(function() {
+    navs[0].onclick = function() {
+        data.sort(function(n1, n2) {
+            n1 = parseInt(n1.title);
+            n2 = parseInt(n2.title);
+            return isReverse ? n2 - n1 : n1 - n2;
+        });
+        render();
+        isReverse = !isReverse;
+        this.innerHTML = isReverse ? "从大到小" : "从小到大";
+    };
+
+    navs[1].onclick = function() {
+        data.sort(function(n1, n2) {
+            n1 = parseInt(n1.title);
+            n2 = parseInt(n2.title);
             return Math.random() - .5;
         });
-        lisArr.forEach(function(element, index) {
-            ul.append(element);
-        });
+        render();
     }
-
 })();
